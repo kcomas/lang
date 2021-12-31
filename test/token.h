1b7991adccc00f1e9fea3715aad91518dba6d3cd
kcomas <kevin.comas.git@gmail.com>
2021-12-31 17:55:18 -0500

    token test header

---
+++ test/token.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include "test.h"
+#include "../src/token.h"
+
+#define TOKEN_TEST_INIT(STR) token t; \
+    token_init(&t); \
+    TOKEN_ASSERT(t, END, 0, 0, 0, 0); \
+    token_state ts; \
+    token_state_init(&ts); \
+    char *str = STR;
+
+#define TOKEN_FOUND() do { \
+    if (token_next(&ts, &t, str) != TOKEN_STATUS_PFX(OK)) TEST_FAIL(); \
+} while (0)
+
+#define TOKEN_ASSERT(TOKEN, TYPE, LINE_NO, CHAR_NO, START_POS, END_POS) do { \
+    if (TOKEN.type != TOKEN_PFX(TYPE) || TOKEN.line_no != LINE_NO || TOKEN.char_no != CHAR_NO || TOKEN.start_pos != START_POS|| TOKEN.end_pos != END_POS) TEST_FAIL(); \
+} while (0)
