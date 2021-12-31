385ba55c3d9ff8f13a7193daecd433100de64db3
kcomas <kevin.comas.git@gmail.com>
2021-12-31 18:04:52 -0500

    remove manual token pass to test

--- test/token.c
+++ test/token.c
@@ -4,21 +4,21 @@
 TEST(add_assign) {
     TOKEN_TEST_INIT("abc: 1 + 2 * 3");
     TOKEN_FOUND();
-    TOKEN_ASSERT(t, VAR, 0, 0, 0, 2);
+    TOKEN_ASSERT(VAR, 0, 0, 0, 2);
     TOKEN_FOUND();
-    TOKEN_ASSERT(t, ASSIGN, 0, 3, 3, 3);
+    TOKEN_ASSERT(ASSIGN, 0, 3, 3, 3);
     TOKEN_FOUND();
-    TOKEN_ASSERT(t, INT, 0, 5, 5, 5);
+    TOKEN_ASSERT(INT, 0, 5, 5, 5);
     TOKEN_FOUND();
-    TOKEN_ASSERT(t, ADD, 0 , 7, 7, 7);
+    TOKEN_ASSERT(ADD, 0 , 7, 7, 7);
     TOKEN_FOUND();
-    TOKEN_ASSERT(t, INT, 0 , 9, 9, 9);
+    TOKEN_ASSERT(INT, 0, 9, 9, 9);
     TOKEN_FOUND();
-    TOKEN_ASSERT(t, MUL, 0 , 11, 11, 11);
+    TOKEN_ASSERT(MUL, 0 , 11, 11, 11);
     TOKEN_FOUND();
-    TOKEN_ASSERT(t, INT, 0 , 13, 13, 13);
+    TOKEN_ASSERT(INT, 0, 13, 13, 13);
     TOKEN_FOUND();
-    TOKEN_ASSERT(t, END, 0 , 14, 14, 14);
+    TOKEN_ASSERT(END, 0 , 14, 14, 14);
 }
 
 INIT_TESTS(
--- test/token.h
+++ test/token.h
@@ -6,7 +6,7 @@
 
 #define TOKEN_TEST_INIT(STR) token t; \
     token_init(&t); \
-    TOKEN_ASSERT(t, END, 0, 0, 0, 0); \
+    TOKEN_ASSERT(END, 0, 0, 0, 0); \
     token_state ts; \
     token_state_init(&ts); \
     char *str = STR;
@@ -15,6 +15,6 @@
     if (token_next(&ts, &t, str) != TOKEN_STATUS_PFX(OK)) TEST_FAIL(); \
 } while (0)
 
-#define TOKEN_ASSERT(TOKEN, TYPE, LINE_NO, CHAR_NO, START_POS, END_POS) do { \
-    if (TOKEN.type != TOKEN_PFX(TYPE) || TOKEN.line_no != LINE_NO || TOKEN.char_no != CHAR_NO || TOKEN.start_pos != START_POS|| TOKEN.end_pos != END_POS) TEST_FAIL(); \
+#define TOKEN_ASSERT(TYPE, LINE_NO, CHAR_NO, START_POS, END_POS) do { \
+    if (t.type != TOKEN_PFX(TYPE) || t.line_no != LINE_NO || t.char_no != CHAR_NO || t.start_pos != START_POS|| t.end_pos != END_POS) TEST_FAIL(); \
 } while (0)
