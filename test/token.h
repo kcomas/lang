b499968234bab3ee4f2fbbfe9520bc781f881d2e
kcomas <kevin.comas.git@gmail.com>
2021-12-31 22:36:44 -0500

    update test to assume token found on next

--- test/token.h
+++ test/token.h
@@ -6,7 +6,6 @@
 
 #define TOKEN_TEST_INIT(STR) token t; \
     token_init(&t); \
-    TOKEN_ASSERT(END, 0, 0, 0, 0); \
     token_state ts; \
     token_state_init(&ts); \
     char *str = STR;
@@ -16,5 +15,6 @@
 } while (0)
 
 #define TOKEN_ASSERT(TYPE, LINE_NO, CHAR_NO, START_POS, END_POS) do { \
+    TOKEN_FOUND(); \
     if (t.type != TOKEN_PFX(TYPE) || t.line_no != LINE_NO || t.char_no != CHAR_NO || t.start_pos != START_POS || t.end_pos != END_POS) TEST_FAIL(); \
 } while (0)
