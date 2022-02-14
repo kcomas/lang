f3259c6ed8a42cfbce2b7030e6f0e6a78b822d1b
kcomas <kevin.comas.git@gmail.com>
2022-02-13 19:41:13 -0500

    file and bracket fix

---
+++ src/file.c
@@ -0,0 +1,12 @@
+
+#include "file.h"
+
+file_status file_to_c_string(const char *const filename, char **const str) {
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) return FILE_STATUS_PFX(CANNOT_OPEN);
+    struct stat sb;
+    if (fstat(fd, &sb) == -1) return FILE_STATUS_PFX(CANNOT_STAT);
+    *str = calloc(1, sizeof(char) * sb.st_size + 1); // null term
+    if (read(fd, *str, sb.st_size) == -1) return FILE_STATUS_PFX(CANNOT_READ);
+    return FILE_STATUS_PFX(OK);
+}
---
+++ src/file.h
@@ -0,0 +1,18 @@
+
+#pragma once
+
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define FILE_STATUS_PFX(NAME) FILE_STATUS_##NAME
+
+typedef enum {
+    FILE_STATUS_PFX(OK),
+    FILE_STATUS_PFX(CANNOT_OPEN),
+    FILE_STATUS_PFX(CANNOT_STAT),
+    FILE_STATUS_PFX(CANNOT_READ)
+} file_status;
+
+file_status file_to_c_string(const char *const filename, char **const str);
