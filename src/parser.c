a8dcb80294f9b6bf29fd166b600211b1cc1f32fc
kcomas <kevin.comas.git@gmail.com>
2022-02-23 03:46:19 -0500

    init parser lists

--- src/parser.c
+++ src/parser.c
@@ -1,17 +1,12 @@
 
 #include "parser.h"
 
+inline void parser_node_list_add_item(parser_node_list *const nl, const parser_node *const node);
+
 extern inline void parser_state_init(parser_state *const ps, const char *const str);
 
-parser_status parser_token_get(parser_state *const ps, token *const t, bool ignore_nl, bool advence) {
-    for (;;) {
-        if ((ps->t_status = token_get(ps->t_state, t, ps->str, advance)) != TOKEN_STATUS_PFX(OK))
-            return PARSER_STATUS_PFX(TOKENIZER_ERROR);
-        if (t->type != TOKEN_PFX(COMMENT) && (ignore_nl == true && t->type != TOKEN_PFX(NEWLINE)) break;
-    }
-    return PARSER_STATUS_PFX(OK);
-}
+extern inline parser_status parser_token_get(parser_state *const ps, token *const t, bool ignore_nl, bool advance);
 
 extern inline parser_status parser_token_next(parser_state *const ps, bool ignore_nl);
 
-extern inline parser_status parser_token_next(parser_state *const ps, bool ignore_nl);
+extern inline parser_status parser_token_peek(parser_state *const ps, bool ignore_nl);
--- src/parser.h
+++ src/parser.h
@@ -16,7 +16,26 @@ typedef enum {
 
 typedef struct _parser_node parser_node;
 
-typedef struct _parser_node_list parser_node_list;
+typedef struct _parser_node_list_item {
+    const parser_node *node;
+    struct _parser_node_list_item *next;
+} parser_node_list_item;
+
+typedef struct _parser_node_list {
+    parser_node_list_item *head, *tail;
+} parser_node_list;
+
+inline void parser_node_list_add_item(parser_node_list *const nl, const parser_node *const node) {
+    parser_node_list_item *item = calloc(1, sizeof(parser_node_list_item));
+    item->node = node;
+    if (nl->head == NULL) {
+        nl->head = item;
+        nl->tail = nl->head;
+    } else {
+        nl->tail->next = item;
+        nl->tail = nl->tail->next;
+    }
+}
 
 typedef struct {
     size_t len; // len does not include null term
@@ -28,7 +47,7 @@ typedef struct {
 typedef struct {
     size_t num_args;
     parser_node *return_type;
-    parser_node_list arg_types;
+    parser_node_list args;
     parser_node_list body;
 } parser_node_fn;
 
@@ -38,18 +57,9 @@ typedef union {
 } parser_node_data;
 
 typedef struct _parser_node {
-    parser_node_type type;
-    parser_node_data data;
+    const parser_node_type type;
+    const parser_node_data data;
 } parser_node;
-
-typedef struct _parser_node_list_item {
-    parser_node *node;
-    struct _parser_node_list_item *next;
-} parser_node_list_item;
-
-typedef struct _parser_node_list {
-    parser_node_list_item *head, *tail;
-} parser_node_list;
 
 #define PARSER_STATUS_PFX(NAME) PARSER_STATUS_PFX_##NAME
 
@@ -67,19 +77,27 @@ typedef struct {
 
 inline void parser_state_init(parser_state *const ps, const char *const str) {
     ps->t_status = TOKEN_STATUS_PFX(OK);
-    parser->n = calloc(1, sizeof(token));
-    token_init(parser->n);
-    parser->p = calloc(1, sizeof(token));
-    token_init(parser->p);
     token_state_init(&ps->t_state);
+    ps->n = calloc(1, sizeof(token));
+    token_init(ps->n);
+    ps->p = calloc(1, sizeof(token));
+    token_init(ps->p);
+    ps->str = str;
 }
 
-parser_status parser_token_get(parser_state *const ps, token *const t, bool ignore_nl, bool advence);
+inline parser_status parser_token_get(parser_state *const ps, token *const t, bool ignore_nl, bool advance) {
+    for (;;) {
+        if ((ps->t_status = token_get(&ps->t_state, t, ps->str, advance)) != TOKEN_STATUS_PFX(OK))
+            return PARSER_STATUS_PFX(TOKENIZER_ERROR);
+        if (t->type != TOKEN_PFX(COMMENT) && (ignore_nl == true && t->type != TOKEN_PFX(NEWLINE))) break;
+    }
+    return PARSER_STATUS_PFX(OK);
+}
 
 inline parser_status parser_token_next(parser_state *const ps, bool ignore_nl) {
     return parser_token_get(ps, ps->n, ignore_nl, true);
 }
 
-inline parser_status parser_token_next(parser_state *const ps, bool ignore_nl) {
+inline parser_status parser_token_peek(parser_state *const ps, bool ignore_nl) {
     return parser_token_get(ps, ps->p, ignore_nl, false);
 }
