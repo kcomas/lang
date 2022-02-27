0a966bd9712ed469be8d2b202ce7dea0b219ef1d
kcomas <kevin.comas.git@gmail.com>
2022-02-27 00:04:30 -0500

    assert parser test reached null byte

--- test/parser.c
+++ test/parser.c
@@ -6,8 +6,9 @@ TEST(arith_with_comment) {
     parser_state ps;
     parser_status status;
     parser_state_init(&ps, str);
-    parser_node *root ;
+    parser_node *root;
     if ((status = parser_parse_exp(&ps, &root)) != PARSER_STATUS_PFX(OK)) TEST_FAIL();
+    if (ps.tn->type != TOKEN_PFX(END)) TEST_FAIL();
 }
 
 INIT_TESTS(
